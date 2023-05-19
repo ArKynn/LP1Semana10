32c76cd6e40760378f9218a46efb77e7765c1f83
David Mendes <david.f.mendes@proton.me>
2023-05-19 14:30:43 +0100

    Fixed CompareTo method and overwritten GetHashCode and Equals Methods

--- LootSort/Loot.cs
+++ LootSort/Loot.cs
@@ -32,11 +32,40 @@ namespace LootSort
 
         public int CompareTo(Loot other)
         {
+            int order = 0;
             if (other == null) return 1;
-            if (other.Kind != Kind) return Kind.CompareTo(other.Kind);
-            if (other.Value != Value) return Value.CompareTo(other.Value);
-            return Description.CompareTo(other.Description);
+            
+            order = Kind.ToString().CompareTo(other.Kind.ToString());
+            
+            if (order == 0)
+            {
+                order = -Value.CompareTo(other.Value);
+            }
 
+            if (order == 0)
+            {
+                order = String.Compare(Description, other.Description, StringComparison.Ordinal);
+            }
+
+            return order;
+
+
+        }
+            
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Kind, Description, Value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Loot otherLoot = obj as Loot;
+            if (obj == null) return false;
+            if (Kind.Equals(otherLoot.Kind) && Value.Equals(otherLoot.Value) && Description.Equals(otherLoot.Description))
+            {
+                return true;
+            }
+            return false;
         }
 
         /// <summary>
