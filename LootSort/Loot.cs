8157c46fe36bbb77452714669e7ffa228f1753de
David Mendes <david.f.mendes@proton.me>
2023-05-19 13:16:06 +0100

    Now implements IComparable and added CompareTo method

--- LootSort/Loot.cs
+++ LootSort/Loot.cs
@@ -1,10 +1,12 @@
+using System;
+
 namespace LootSort
 {
     /// <summary>
     /// The Loot class should implement IComparable<Loot>
     /// and override GetHashCode() and Equals()
     /// </summary>
-    public class Loot
+    public class Loot : IComparable<Loot>
     {
         /// <summary>Type of loot.</summary>
         public LootType Kind { get; }
@@ -26,6 +28,15 @@ namespace LootSort
             Kind = kind;
             Description = description;
             Value = value;
+        }
+
+        public int CompareTo(Loot other)
+        {
+            if (other == null) return 1;
+            if (other.Kind != Kind) return Kind.CompareTo(other.Kind);
+            if (other.Value != Value) return Value.CompareTo(other.Value);
+            return Description.CompareTo(other.Description);
+
         }
 
         /// <summary>
