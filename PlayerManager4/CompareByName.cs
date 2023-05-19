54e910734329bee64561afb0f633d9d2a7e33e38
David Mendes <david.f.mendes@proton.me>
2023-05-19 14:47:29 +0100

    Added CompareByName class

---
+++ PlayerManager4/CompareByName.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+
+namespace PlayerManager4;
+
+public class CompareByName : IComparer<Player>
+{
+    public int SortingOrder { get; private set; }
+
+    public CompareByName([Optional] bool sortingOrder)
+    {
+        SortingOrder = -1 * Convert.ToInt16(sortingOrder);
+    }
+
+    public int Compare(Player? x, Player? y)
+    {
+        if (y == null) return 1;
+        if (SortingOrder == -1) return x.Score.CompareTo(y.Score);
+        else if (SortingOrder == 0) return String.Compare(x.Name, y.Name, StringComparison.Ordinal);
+        return -String.Compare(x.Name, y.Name,StringComparison.Ordinal);
+    }
+    
+}
